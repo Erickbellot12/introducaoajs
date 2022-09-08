a13b208f49d355c467811c7d61c62dafeae3df1a
Erickbellot12 <111981561+Erickbellot12@users.noreply.github.com>
2022-09-08 08:55:14 -0300

    Add files via upload

---
+++ 06_09_22/javaScript/anotacoes.js
@@ -0,0 +1,35 @@
+/*
+javasscript - clienr- side
+
+-html é onde eu coloco o meu código em javascript.
+-o java escript é popular por ser usado em mais de 90% das páginas web
+-CSS é onde eu estilizo minha página html
+-javascript é onde eu dou funcionalidade a página.
+
+
+console.log("") -> tem a mesma função do "escreva" do portugol
+
+----------
+
+dom - document object model
+é la que eu consigo estruturar todo o meu documento.
+
+o html é o "que" o dom é o "como".
+------
+váriaveis em javaescript
+
+existem 3 formas simples de se declarar variaveis no js 
+
+var & let = para váriaveis que mudam com o tempo
+
+const = para variaveis que sao constantes
+
+tipagem dinâmica - o javascript consegue reconhecer automáticamente o
+tipo  de cada variável, sem que eu precise especificar nos minimos detalhes
+
+
+
+
+
+
+*/
---
+++ 06_09_22/javaScript/primeiraquestão/script.js
@@ -0,0 +1,8 @@
+const lampada = document.getElementById("lamp");
+
+function minhaFuncaoAcende(){
+     lamp.src = "WIN_20220906_10_15_51_Pro.jpg";
+}
+     function minhaFuncaoApaga(){
+        lamp.src = "lampOff.jpg"
+     }
